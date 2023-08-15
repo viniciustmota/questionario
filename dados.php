02c494a68238bb5dca6c70a27283253ddd8f002a
unknown <motaviny140@gmail.com>
2023-08-15 12:24:10 -0300

    Finalizada pra mobile, terminando para tablet

--- dados.php
+++ dados.php
@@ -1,21 +1,72 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dados</title>
+    <link rel="stylesheet" href="./assets/css/estilos.css">
 </head>
+
 <body>
     <?php
-        $nome = $_GET['nome'];
-        $idade = $_GET['idade'];
-        $estado = $_GET['estado'];
-        $pais = $_GET['pais'];
-        $esporte = $_GET['esporte'];
-        $comida = $_GET['comida'];
-        echo($nome." tem $idade anos é nascido em $estado/$pais, seu esporte preferido é $esporte e sua comida preferida é $comida");
-        echo "<img src='./assets/img/$esporte.jpg' \">";
+    $nome = $_GET['nome'];
+    $idade = $_GET['idade'];
+    $sexo = $_GET['sexo'];
+    $esporte = $_GET['esporte'];
+    $tempo = $_GET['tempo'];
+    $alimentacao = $_GET['alimentacao'];
+    $sono = $_GET['sono'];
+    $campeonatos = $_GET['campeonatos'];
     ?>
+
+    <header class="cabecalho">
+        <img src="./assets/img/Logo.svg" alt="Logo Projeto Esporte todos os dias" class="cabecalho__logo">
+        <p class="cabecalho__titulo">Projeto Esporte Todos os dias</p>
+    </header>
+
+    <section class="banner banner-dados">
+        <h1 class="banner__titulo">
+            <?php echo ("Que legal $nome!") ?>
+        </h1>
+        <p class="banner__descricao banner__descricao-dados">
+            <?php echo ("Você pratica $esporte,<br>
+há $tempo anos") ?>
+        </p>
+    </section>
+
+    <section class="dados">
+        <?php echo "<img class='dados__img' src='./assets/img/$esporte.jpg' \">";
+        echo ("<h2 class='dados__titulo'>Dados sobre você</h2>
+    <ul class='dados__lista'>
+    <li class='dados__item'>Gênero $sexo;</li>
+    <li class='dados__item'>Tem $idade anos</li>
+    <li class='dados__item'>Ja participou em $campeonatos campeonatos;</li>
+    <li class='dados__item'>Se alimenta bem: $alimentacao;</li>
+    <li class='dados__item'>Dorme $sono horas por dia;</li>
+    </ul>
+    ");
+
+        ?>
+    </section>
+
+    <footer class="rodape">
+        <p class="rodape__texto">Grupo Mota</p>
+        <section class="rodape__topicos">
+            <div class="rodape__servicos">
+                <p class="rodape__titulo">Empresas</p>
+                <a href="#" class="rodape__item"><img src="./assets/img/mota informatica.svg" alt="Logo Mota Informatica">Mota Informatica</a>
+                <a href="#" class="rodape__item"><img src="./assets/img/vettel.svg" alt="Logo Vettel SafeDrive">Vettel SafeDrive</a>
+            </div>
+
+            <div class="rodape__servicos">
+                <p class="rodape__titulo">Projetos Sociais</p>
+                <a href="#" class="rodape__item"><img src="./assets/img/Juntos pelo Mar.svg" alt="Logo Projeto Juntos pelo Mar">Juntos pelo Mar</a>
+                <a href="#" class="rodape__item"><img src="./assets/img/Esporte todos os dias.svg" alt="Logo Projeto Esporte todos os dias"> Esporte todos os dias</a>
+            </div>
+        </section>
+    </footer>
 </body>
+
 </html>
--- questionario.php
+++ questionario.php
@@ -8,31 +8,40 @@
     <title>Questionario</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="./assets/estilos/estilos.css">
+    <link rel="stylesheet" href="./assets/css/estilos.css">
 </head>
 
 <body>
-    <section class="container">
-        <h1 class="container__titulo">Questionario</h1>
-        <p class="container__descricao">Informe seus dados</p>
+    <header class="cabecalho">
+        <img src="./assets/img/Logo.svg" alt="Logo Projeto Esporte todos os dias" class="cabecalho__logo">
+        <p class="cabecalho__titulo">Projeto Esporte Todos os dias</p>
+    </header>
+
+    <section class="banner">
+        <h1 class="banner__titulo">
+            Esportes são vida!
+        </h1>
+        <p class="banner__descricao">
+            Conte nos sobre você e sua vida esportiva!
+        </p>
     </section>
 
     <section class="questionario">
-        <form action="dados.php" method="get">
-            <label for="nome" class="questionario__label">Nome:</label>
-            <input type="text" id="nome" name="nome" placeholder="digite seu nome" class="questionario__resposta">
+        <h2 class="questionario__titulo">Questionario</h2>
+        <form action="dados.php" method="get" class="questionario__form">
+            <label for="nome" class="questionario__label">Qual o seu nome?</label>
+            <input type="text" id="nome" name="nome" placeholder="digite seu nome" class="questionario__resposta" required>
 
-            <label for="idade" class="questionario__label">Idade:</label>
-            <input type="text" id="idade" name="idade" placeholder="digite sua idade" class="questionario__resposta">
+            <label for="idade" class="questionario__label">Qual a sua idade?</label>
+            <input type="number" id="idade" name="idade" placeholder="digite sua idade" class="questionario__resposta" required>
 
-            <label for="pais" class="questionario__label">Pais:</label>
-            <input type="text" id="pais" name="pais" placeholder="digite seu pais" class="questionario__resposta">
-
-            <label for="estado" class="questionario__label">Estado:</label>
-            <input type="text" id="estado" name="estado" placeholder="digite seu estado" class="questionario__resposta">
-
-            <label for="esporte" class="questionario__label">Esporte preferido:</label>
-            <select name="esporte" id="">
+            <label for="sexo" class="questionario__label">Qual o seu sexo?</label>
+            <div class="questionario__radiobtn">
+                <input type="radio" name="sexo" id="sexo-m" class="questionario__resposta" value="Masculino" required> <label for="sexo-m">Masculino</label>
+                <input type="radio" name="sexo" id="sexo-f" class="questionario__resposta" value="Feminino" required> <label for="sexo-f">Feminino</label>
+            </div>
+            <label for="esporte" class="questionario__label">Qual seu esporte favorito?</label>
+            <select name="esporte" id="" class="questionario__resposta">
                 <option value="Atletismo">Atletismo</option>
                 <option value="Automobilismo">Automobilismo</option>
                 <option value="Badminton">Badminton</option>
@@ -93,7 +102,7 @@
                 <option value="Natação">Natação</option>
                 <option value="Parapente">Parapente</option>
                 <option value="Patinação">Patinação</option>
-                
+
                 <option value="Patinação Artística no Gelo">Patinação Artística no Gelo</option>
                 <option value="Patinação de Velocidade no Gelo">Patinação de Velocidade no Gelo</option>
                 <option value="Polo Aquático">Polo Aquático</option>
@@ -126,16 +135,39 @@
                 <option value="Xadrez">Xadrez</option>
             </select>
 
-            <!-- <input type="text" id="esporte" name="esporte" placeholder="digite seu esporte preferido" class="questionario__resposta"> -->
+            <label for="tempo" class="questionario__label">Há quanto tempo você pratica?</label>
+            <input type="number" id="tempo" name="tempo" placeholder="digite quantos anos você pratica" class="questionario__resposta" required>
 
-            <label for="comida" class="questionario__label">Comida preferida:</label>
-            <input type="text" id="comida" name="comida" placeholder="digite sua comida preferida" class="questionario__resposta">
+            <label for="campeonatos" class="questionario__label">Quantas vezes participou de campeonatos?</label>
+            <input type="number" id="campeonatos" name="campeonatos" placeholder="Digite quantas vezes você participou" class="questionario__resposta" required>
+
+            <label for="alimentação" class="questionario__label">Você se alimenta bem?</label>
+            <div class="questionario__radiobtn">
+                <input type="radio" name="alimentacao" id="alimentacao-sim" class="questionario__resposta" value="Sim" required> <label for="alimentacao-sim">Sim</label>
+                <input type="radio" name="alimentacao" id="alimentacao-nao" class="questionario__resposta" value="Não" required><label for="alimentacao-nao">Não</label>
+            </div>
+
+            <label for="sono" class="questionario__label">Você dorme quantas horas por dia?</label>
+            <input type="number" id="sono" name="sono" placeholder="Digite quantas horas você dorme por dia" class="questionario__resposta" required>
 
             <input type="submit" value="Enviar" class="questionario__btn"></input>
         </form>
     </section>
     <footer class="rodape">
-        <p class="rodape__copyrigth">&copy;Mota</p>
+        <p class="rodape__texto">Grupo Mota</p>
+        <section class="rodape__topicos">
+            <div class="rodape__servicos">
+                <p class="rodape__titulo">Empresas</p>
+                <a href="#" class="rodape__item"><img src="./assets/img/mota informatica.svg" alt="Logo Mota Informatica">Mota Informatica</a>
+                <a href="#" class="rodape__item"><img src="./assets/img/vettel.svg" alt="Logo Vettel SafeDrive">Vettel SafeDrive</a>
+            </div>
+
+            <div class="rodape__servicos">
+                <p class="rodape__titulo">Projetos Sociais</p>
+                <a href="#" class="rodape__item"><img src="./assets/img/Juntos pelo Mar.svg" alt="Logo Projeto Juntos pelo Mar">Juntos pelo Mar</a>
+                <a href="#" class="rodape__item"><img src="./assets/img/Esporte todos os dias.svg" alt="Logo Projeto Esporte todos os dias"> Esporte todos os dias</a>
+            </div>
+        </section>
     </footer>
 </body>
 
